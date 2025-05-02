0a8b6dfa4e7f1727841b485913f384eb19208b3a
Ankesh1A <ankeshbarahdiya05112002@gmail.com>
2025-05-02 16:57:09 +0530

    ...

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),],
-})
+    tailwindcss(),
+  ],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    // This ensures React Router paths work in dev mode
+    historyApiFallback: true,
+  },
+});
