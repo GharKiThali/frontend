73c9516b6462bc8888093dd9b9e3279f0dee9e8c
Ankesh1A <ankeshbarahdiya05112002@gmail.com>
2025-05-13 17:15:21 +0530

    change url

--- src/instant/axios.js
+++ src/instant/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
- baseURL:'http://localhost:8080', // Backend URL
+ baseURL:'https://backend-y8ah.onrender.com', // Backend URL
   withCredentials: true, // Cookies ko bhejne ke liye
 });
 
